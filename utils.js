500d23ed0717c93945cb59c5562dd3a3c16d42b1
estevaoanjo <estevaosilvino25@gmail.com>
2026-01-17 21:49:16 -0300

    Add controls of the game

--- utils.js
+++ utils.js
@@ -1,2 +1,9 @@
-console.log("Utilidade Nova");
-console.log("Nova linha");
+
+var contador = 0;
+
+while (contador < 10){
+
+   console.log("Utilidade Nova", contador);
+   contador = contador+1;
+
+}
