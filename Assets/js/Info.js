36ddff23a50c5a1a1b63cde44182129ffac006e6
estevaoanjo <estevaosilvino25@gmail.com>
2026-03-04 23:41:16 -0300

    New Rules of the game

--- Assets/js/Info.js
+++ Assets/js/Info.js
@@ -1,9 +1,19 @@
 function mostrarRegras() {
   console.log(
-    "Velocidade do Computador: " +
+    "REGRAS:\n\n",
+    " 1. A bola rebate nas laterais do campo, invertendo a direção.\n\n",
+    " 2. Ao bater nas raquetes, a bola é rebatida com efeito, indo para o sentido oposto.\n\n",
+    " 3. Quando a bola ultrapassa a linha do adversário:\n\n",
+     "  3.1 O jogador adversário ganha 1 ponto.\n\n",
+     "  3.2 A bola é centralizada novamente, indo para o jogador que marcou o ponto.\n\n",
+    " 4. Quando um jogador atingir 5 de vantagem, o jogo é reiniciado\n\n",
+     "  4.1 Se o jogador com a vantagem for o computador, sua velocidade é diminuida em -0,5 da sua velocidade atual (ele inicia em velocidade: 3)\n\n",
+     "  4.2 Se o jogador com a vantagem for o jogador 1(Você), a velocidade do jogador 2 (computador) é aumentada em +0,5 da sua velocidade inicial (sendo 20 sua velocidade limite)\n\n",
+    "Estastisticas do jogo\n",
+    "Velocidade do Computador:" +
       velojg2 +
       " | Velocidade da Bola: " +
-      velocidadeBolaX,
+      velocidadeBolaY,
   );
 }
 
