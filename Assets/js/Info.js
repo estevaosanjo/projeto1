7dad97b14fead8941d798e1eb71ea48cb30e4f57
estevaoanjo <estevaosilvino25@gmail.com>
2026-03-22 03:01:48 -0300

    Pop-up modal - Regras do jogo

--- Assets/js/Info.js
+++ Assets/js/Info.js
@@ -9,14 +9,13 @@ function mostrarRegras() {
     " 4. Quando um jogador atingir 5 de vantagem, o jogo é reiniciado\n\n",
      "  4.1 Se o jogador com a vantagem for o computador, sua velocidade é diminuida em -0,5 da sua velocidade atual (ele inicia em velocidade: 3)\n\n",
      "  4.2 Se o jogador com a vantagem for o jogador 1(Você), a velocidade do jogador 2 (computador) é aumentada em +0,5 da sua velocidade inicial (sendo 20 sua velocidade limite)\n\n",
-    "Estastisticas do jogo\n",
-    "Velocidade do Computador:" +
-      velojg2 +
-      " | Velocidade da Bola: " +
-      velocidadeBolaY,
+
   );
 }
+
 
 document
   .getElementById("botao-regras")
   .addEventListener("click", mostrarRegras);
+
+
